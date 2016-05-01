abaf919d86f23a7c7e5bcef7600793cd395c6d21
JackeyChen <jackeychen1015@163.com>
2016-05-01 12:59:09 +0800

    添加网络工具类

---
+++ JCNetWorkToll.h
@@ -0,0 +1,31 @@
+//
+//  JCNetWorkToll.h
+//  06-【网络成功和失败的回调】
+//
+//  Created by 陈鹏 on 16/1/13.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//定义Block 类型:
+//1.成功回调 参数类型: 二进制数据和响应头信息
+typedef void(^SuccessBlcok)(NSData *data , NSURLRequest *request);
+//2.失败回调
+typedef void(^FailBlock)(NSError *error);
+
+@interface JCNetWorkToll : NSObject
+
+
+//实例化单例的方法
++(instancetype)shareNetWorkTool;
+
+//发送网络请求的方法
+//urlString:网路接口
+//两个block 参数
+//1.successBlk 成功回调
+//2.failBlk 失败回调
+-(void)getServerDataWithUrlString:(NSString *)urlString Success:(SuccessBlcok)successBlk Fail:(FailBlock)failBlk;
+
+
+@end
---
+++ JCNetWorkToll.m
@@ -0,0 +1,63 @@
+//
+//  JCNetWorkToll.m
+//  06-【网络成功和失败的回调】
+//
+//  Created by 陈鹏 on 16/1/13.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import "JCNetWorkToll.h"
+
+@implementation JCNetWorkToll
+
+//网络请求完成之后的回调
+-(void)getServerDataWithUrlString:(NSString *)urlString Success:(SuccessBlcok)successBlk Fail:(FailBlock)failBlk{
+    
+//    1.创建网络请求
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:15];
+    
+//    2.发送网络请求
+    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+//        网络请求完成之后的回调
+        
+//        成功:
+        if (data && !error) {
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+//               3.执行成功回调
+                if (successBlk) {
+                    successBlk(data,request);
+                }
+            });
+        }else{
+            
+            if (failBlk) {
+//                3.执行失败回调
+                failBlk(error);
+            }
+        }
+    }]resume];
+
+}
+
+
++(instancetype)shareNetWorkTool{
+    
+    static id _instance;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        _instance = [[self alloc] init];
+        
+    });
+    
+    return _instance;
+}
+
+
+@end
